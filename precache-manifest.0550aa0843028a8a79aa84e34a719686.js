7e29d12eaf9b8494ba18524dec8a7819871e9669
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-08-20 09:44:15 +0530

    Updates

---
+++ precache-manifest.0550aa0843028a8a79aa84e34a719686.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49828096e35bc399327287d24ae5aed",
+    "url": "/mofn/index.html"
+  },
+  {
+    "revision": "beb701bc23fae333351e",
+    "url": "/mofn/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a13073258cc9425db3d1",
+    "url": "/mofn/static/css/main.4cddfa92.chunk.css"
+  },
+  {
+    "revision": "beb701bc23fae333351e",
+    "url": "/mofn/static/js/2.50c942c0.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/mofn/static/js/2.50c942c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13073258cc9425db3d1",
+    "url": "/mofn/static/js/main.6e62f404.chunk.js"
+  },
+  {
+    "revision": "ec75b666e28505b6f0d7",
+    "url": "/mofn/static/js/runtime-main.2b41dba2.js"
+  },
+  {
+    "revision": "23e2bf0545e292f09e98201d736a3a58",
+    "url": "/mofn/static/media/img1.23e2bf05.jpg"
+  },
+  {
+    "revision": "55618e5c36b0e5b55ca77f95691d0f7f",
+    "url": "/mofn/static/media/jkdb1.55618e5c.jpg"
+  },
+  {
+    "revision": "d87a31d5c24d6315bceda46467c42e63",
+    "url": "/mofn/static/media/jkdb10.d87a31d5.jpg"
+  },
+  {
+    "revision": "5b5a5692b3b6cdfbb5059deff6d1fe73",
+    "url": "/mofn/static/media/jkdb11.5b5a5692.jpg"
+  },
+  {
+    "revision": "5414b23bfc1134e08604a9b23ba70dd6",
+    "url": "/mofn/static/media/jkdb14.5414b23b.png"
+  },
+  {
+    "revision": "2c246782e8e7063cb92f5339ced89d3f",
+    "url": "/mofn/static/media/jkdb15.2c246782.jpg"
+  },
+  {
+    "revision": "04eb7b0a61247930890e03db3bd0b9fd",
+    "url": "/mofn/static/media/jkdb16.04eb7b0a.jpg"
+  },
+  {
+    "revision": "eaac2b4da89dbef6f69d34f25daf7c59",
+    "url": "/mofn/static/media/jkdb17.eaac2b4d.jpg"
+  },
+  {
+    "revision": "0aa132d2f14c8d984713c71164db49ac",
+    "url": "/mofn/static/media/jkdb18.0aa132d2.jpg"
+  },
+  {
+    "revision": "006c3ed04d8b7e3c1b4afc18468cba6f",
+    "url": "/mofn/static/media/jkdb2.006c3ed0.jpg"
+  },
+  {
+    "revision": "71d3c42112e86de28fb911fb1380782f",
+    "url": "/mofn/static/media/jkdb3.71d3c421.jpg"
+  },
+  {
+    "revision": "181ad693b4295f215e134bccd2c5d94c",
+    "url": "/mofn/static/media/jkdb4.181ad693.jpg"
+  },
+  {
+    "revision": "fca36cc131e72035a4eed85e8843a28d",
+    "url": "/mofn/static/media/jkdb5.fca36cc1.jpg"
+  },
+  {
+    "revision": "81e4751459281950c331fd93156faf28",
+    "url": "/mofn/static/media/jkdb6.81e47514.jpg"
+  },
+  {
+    "revision": "10ec43aaa76bebd25ea59478ac36074c",
+    "url": "/mofn/static/media/jkdb9.10ec43aa.jpg"
+  }
+]);
